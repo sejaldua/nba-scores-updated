8202a828bffedc348fcb6832cf50a250b68142b2
Sejal Dua <sejaldua@gmail.com>
2024-04-07 20:05:49 -0400

    update scores

--- game_scores.py
+++ game_scores.py
@@ -9,11 +9,11 @@ import requests
 
 gamefinder = leaguegamefinder.LeagueGameFinder()
 games = gamefinder.get_data_frames()[0]
-games = games[(games['SEASON_ID'].isin(['22023'])) & (games['GAME_ID'].str.startswith('00'))]
+games = games[(games['SEASON_ID'].isin(['22023'])) & (games['GAME_ID'].str.startswith('00')) & (games['WL'].isin(['W', 'L']))]
 games['is_home'] = games['MATCHUP'].apply(lambda x: 1 if 'vs.' in x else 0)
 home = games[games['is_home'] == 1][['GAME_ID', 'GAME_DATE', 'TEAM_ABBREVIATION', 'PTS']]
 road = games[games['is_home'] == 0][['GAME_ID', 'GAME_DATE', 'TEAM_ABBREVIATION', 'PTS']]
-merged = pd.merge(home, road, how='left', left_on=['GAME_ID'], right_on=['GAME_ID'])
+merged = pd.merge(road, home, how='left', left_on=['GAME_ID'], right_on=['GAME_ID'])
 merged = merged[['GAME_DATE_x', 'TEAM_ABBREVIATION_x', 'PTS_x', 'TEAM_ABBREVIATION_y', 'PTS_y']]
 merged.columns = ['Date', 'Road', 'Road PTS', 'Home', 'Home PTS']
 merged['Date'] = pd.to_datetime(merged['Date']).dt.tz_localize(None)
